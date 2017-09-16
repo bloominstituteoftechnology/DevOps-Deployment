1568e4ef0e853f7bb2a92e32a1371efac7988c5b
H. Thomson Comer <thomcom@gmail.com>
2017-09-15 18:32:54 -0600

    Updated index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,11 @@
 /*eslint no-unused-vars: "off"*/
 import React from 'react';
 import ReactDom from 'react-dom';
-import request from 'request';
-import request-promise from 'request-promise';
+// removed request and request-promise, they didn't import correctly
+// you can use:
+// axios
+// bluebird
+// request or request-promise
 
 require('!style-loader!css-loader!sass-loader!./index.scss');
 
